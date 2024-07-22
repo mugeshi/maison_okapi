d1104ff12fe75fe6e847eeb5e49de73838f7dea6
mugeshi <nicolemugeshi@gmail.com>
2024-07-22 10:10:45 +0300

    updated NavbarLinks

---
+++ maison_okapi/src/components/Navbar.js
@@ -0,0 +1,22 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h2>maison okapi</h2>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/accessories">Accessories</Link></li>
+        <li><Link to="/footwear">Footwear</Link></li>
+        <li><Link to="/newcollection">New Collection</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
