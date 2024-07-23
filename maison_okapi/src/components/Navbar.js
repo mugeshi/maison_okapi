d83264055d782651716f8a6b65bf8c032211277c
mugeshi <nicolemugeshi@gmail.com>
2024-07-23 23:44:14 +0300

    updated icons

--- maison_okapi/src/components/Navbar.js
+++ maison_okapi/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons'; // Correct icon import
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-         <h2 className="playwrite-at-logo">maison okapi</h2>
+        <h2 className="playwrite-at-logo">maison okapi</h2>
       </div>
       <ul className="navbar-links">
         <li><a href="/">Home</a></li>
@@ -13,6 +15,9 @@ const Navbar = () => {
         <li><a href="/footwear">Footwear</a></li>
         <li><a href="/newcollection">New Collection</a></li>
       </ul>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faUser} className="fa-thin" /> 
+      </div>
     </nav>
   );
 };
